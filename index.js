0df91938913088a6547557bbe528a13dbca529a1
Suprabhat Kumar <jnvsuprabh@gmail.com>
2025-02-08 22:42:11 +0530

    proposal ready

---
+++ index.js
@@ -0,0 +1,34 @@
+const { time } = require('console');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+const path = require('path');
+const currentDate = new Date();
+
+app.use(express.json());
+app.use(express.static('public')); // Serve static files
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+app.get('/response', (req, res) => {
+    res.sendFile(path.join(__dirname, '/', 'response.html'));
+});
+app.post('/save-response', (req, res) => {
+    const responseText = `Response: ${req.body.response} ${currentDate.toLocaleTimeString()}\n`;
+    
+    fs.appendFile('responses.html', responseText, (err) => {
+        if (err) {
+            console.error('Error writing to file', err);
+            res.status(500).send('Error saving response');
+        } else {
+            res.send('Response saved successfully!');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
